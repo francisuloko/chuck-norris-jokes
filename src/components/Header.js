c8b242b62334b1bd3048520bcad683931245df58
francisuloko <francisuloko@yahoo.com>
2022-04-30 02:25:18 +0100

    Add icons to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "../assets/styles/header.scss";
+import ProfileIcon from '../assets/assets_Homework_Front-End_01/shape@3x.png'
+import PathIcon from '../assets/assets_Homework_Front-End_02/path@3x.png'
 
 export default function Header() {
   return (
@@ -12,8 +14,10 @@ export default function Header() {
           <li>
             <a href="#">SPECIAL OFFERS</a>
           </li>
-          <li>
+          <li className="my-space">
+            <img src={ProfileIcon} alt="profile icon" />
             <a href="#">MY SPACE</a>
+            <img src={PathIcon} alt="path icon" />
             <ul>
               <li>
                 <a href="#">My published jokes</a>
@@ -22,7 +26,7 @@ export default function Header() {
                 <a href="#">My saved jokes</a>
               </li>
               <li>
-                <a href="#">Account information</a>
+                <a href="#">Account Information</a>
               </li>
               <li>
                 <a href="#">Publish new joke</a>

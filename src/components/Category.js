29125b5287ce0508b8bc309d64af865b038cdb15
francisuloko <francisuloko@yahoo.com>
2022-04-30 15:42:43 +0100

    Add basic Categories section

---
+++ src/components/Category.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../assets/styles/category.scss';
+
+export default function Category() {
+  return (
+    <div className='categories'>
+      <span>CATEGORY 1</span>
+      <span>CATEGORY 2</span>
+      <span>CATEGORY 3</span>
+      <span>CATEGORY 4</span>
+      <span>CATEGORY 5</span>
+      <span>CATEGORY 1</span>
+      <span>VIEW ALL</span>
+    </div>
+  )
+}

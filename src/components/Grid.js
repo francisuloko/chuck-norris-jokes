2e04d38918672a6bf770ba5aafa414dbecdddf5f
francisuloko <francisuloko@yahoo.com>
2022-05-02 08:26:01 +0100

    Add basic jokes grid structure

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import '../assets/styles/grid.scss';
 
-export default function Grid({ children }) {
+export default function Grid(props) {
   return (
     <div className='grid'>
-      { children }
+      { props.gridItems }
     </div>
   )
 }

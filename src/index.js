2b2109a99eb9f6cf5f2e1c22817cbd64fa87b2ec
francisuloko <francisuloko@yahoo.com>
2022-05-04 00:18:30 +0100

    Import ReactDOM from react-dom client

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import './index.css';
+import './index.scss';
 
-ReactDOM.render(<App />, document.getElementById("app"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
